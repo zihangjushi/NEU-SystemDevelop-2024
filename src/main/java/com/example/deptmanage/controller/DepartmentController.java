b41a8161be07a4615b2ca004f786e19fe487830e
wangy <1106081245@qq.com>
2024-07-02 00:05:51 +0800

    新增一些后端接口

--- src/main/java/com/example/deptmanage/controller/DepartmentController.java
+++ src/main/java/com/example/deptmanage/controller/DepartmentController.java
@@ -1,8 +1,7 @@
 package com.example.deptmanage.controller;
 
-import com.example.deptmanage.biz.DepartmentBiz;
-import com.example.deptmanage.entity.Company;
-import com.example.deptmanage.entity.Department;
+import com.example.deptmanage.biz.*;
+import com.example.deptmanage.entity.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +17,8 @@ import java.util.Map;
 public class DepartmentController {
     @Autowired
     private DepartmentBiz departmentBiz;
-    @RequestMapping("/list")
+    private CompanyBiz companyBiz;
+    @RequestMapping("/deptlist")
     public Map listDepartment(){
         List<Department> list = departmentBiz.getDepartmentList();
         Map map = new HashMap();
@@ -39,14 +39,31 @@ public class DepartmentController {
     public Map searchByCompanyId(int id){
         System.out.println(id);
         List<Department> deptlist = departmentBiz.searchByCompanyId(id);
-
         Map map = new HashMap();
-        map.put("isOk",true);
-        map.put("deptlistByCompany",deptlist);
-        map.put("msg","查询成功");
+        if(deptlist.size() == 0){
+            map.put("isOk", false);
+        }
+        else{
+            map.put("isOk",true);
+            map.put("deptlistByCompany",deptlist);
+            map.put("total", deptlist.size());
+            map.put("msg","查询成功");
+        }
         return map;
     }
 
+    @RequestMapping("/list")
+    public Map list(){
+        List<Department> deptlist = departmentBiz.getDepartmentList();
+        List<Company> companyList = companyBiz.getCompanyList();
+        Map map = new HashMap();
+        map.put("isOk",true);
+        map.put("departments",deptlist);
+        map.put("companies", companyList);
+        map.put("total", deptlist.size() + companyList.size());
+        map.put("msg","查询成功");
+        return map;
+    }
 
 
 }
