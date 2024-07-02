d55faf55f79581681acb31f806642e40b8360e79
XwXwy <865549459@qq.com>
2024-07-02 20:48:08 +0800

    123

--- Front_End_Conde/src/views/LoginView.vue
+++ Front_End_Conde/src/views/LoginView.vue
@@ -90,7 +90,7 @@ export default defineComponent({
       });
     },
     register() {
-      this.$router.push('/meeting');
+      this.$router.push('/news');
     },
     refreshCaptcha() {
       // Replace with actual logic to refresh captcha
