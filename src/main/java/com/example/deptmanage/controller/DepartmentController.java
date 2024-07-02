2096db1b2ac55fc6157ea4d638ba4394e2591aa1
wangy <1106081245@qq.com>
2024-07-02 13:57:48 +0800

    新增后端接口

--- src/main/java/com/example/deptmanage/controller/DepartmentController.java
+++ src/main/java/com/example/deptmanage/controller/DepartmentController.java
@@ -3,6 +3,7 @@ package com.example.deptmanage.controller;
 import com.example.deptmanage.biz.*;
 import com.example.deptmanage.entity.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/dept")
+@CrossOrigin(originPatterns = "http://localhost:8081")
 
 public class DepartmentController {
     @Autowired
